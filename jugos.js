b2512f592eae58cc6f974c3834c976a58c4e8580
Brosscom27 <mcordovacsx@gmail.com>
2024-03-12 03:17:21 -0600

    Agregacion de comentarios en el codigo

--- jugos.js
+++ jugos.js
@@ -1,4 +1,6 @@
+//Funcion par acalcular el tiempo necesario para obtener el jugo deseado.
 function timeToMixJuice(juice) {
+    //Utilizacion de una estructura swicht para manejar diferentes tipos de jugos.
     switch (juice) {
         case "Pure Strawberry Joy":
             console.log("Tomara 0.5 minutos");
@@ -17,21 +19,25 @@ function timeToMixJuice(juice) {
             console.log("Tomara 5 minutos");
             return 5;
             break;
+        //Para cual quier otro jugo o ofertas especiales.
         default:
             console.log("Es otro jugo, tardara unos 2.5 minutos");
             return 2.5;
     }
 }
 
+//Objeto con el tamaño de las limas y cantidad de rodajas
 const limes = {
     "pequena": 6,
     "mediana": 8,
     "grande": 10,
 };
 
+//Funcion  para determinar cuantas limas se deben cortar para alcanzar el numero de cortes.
 function limesToCut(numberCuts, limesAvailable) {
     let numberLimes = 0
     let cuts = 0
+    //Iteramos sobre el arreglo de limas disponibles.
     for (let i = 0; i < limesAvailable.length; i++) {
         const size = limesAvailable[i];
         cuts += limes[size];
@@ -41,12 +47,15 @@ function limesToCut(numberCuts, limesAvailable) {
             break;
         }
     }
+    //Si los cortes son insuficientes.
     if (cuts < numberCuts) {
         console.log("No hay suficientes limas, por lo que corta todas");
     }
+    //Retorna el numero de limas a cortar.
     return numberLimes;
 }
 
+//Objeto con las bebidas y su tiempo de preparacion.
 const drinks = {
     "Pure Strawberry Joy": 0.5,
     "Energizer": 1.5,
@@ -55,16 +64,21 @@ const drinks = {
     "All or nothing" : 5,
 };
 
+//Funcion para gestionar las ordenes.
 function ordersLiMei(timeLeft, driksMissing){
+    //Verifica que el tiempo dado no sea 0.
     if (timeLeft > 0){
+        //Mientras haya bebidas ppor hacer.
         while (driksMissing.length > 0){
             const drink = driksMissing.shift();
+            //si la bebida no esta en el objeto toma como una bebida especial por lo que es 2.5
             timeLeft -= drinks[drink] || 2.5;
             if (timeLeft <= 0){
                 console.log("Ya se tiene que ir Li Mei");
                 break;
             }
         }
+        //Retorna las bebidas que faltan por hacer.
         return driksMissing;
     }
     else{
@@ -72,12 +86,13 @@ function ordersLiMei(timeLeft, driksMissing){
     }
 }
 
+//Se llaman a las funciones, se puede cambiar los valores para diferentes resultados
 testJuice = timeToMixJuice("Green Garden");
 console.log(testJuice);
 console.log();
 testLimes = limesToCut(50, ["grande", "pequena", "mediana", "grande", "grande", "grande", "grande", "grande"]);
 console.log(testLimes);
 console.log();
-testTime = ordersLiMei(8,["Vodka", "All or nothing", "Pure Strawberry Joy", "Energizer"]);
+testTime = ordersLiMei(5,["Vodka", "All or nothing", "Pure Strawberry Joy", "Energizer"]);
 console.log("Bebidas faltantes por hacer Dmitry")
 console.log(testTime)
